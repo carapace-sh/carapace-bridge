2e930c1bce0ff6cd88642fd4fcceae69189a1e89
rsteube <rsteube@users.noreply.github.com>
2023-04-01 11:37:19 +0200

    source config directly and don't mess with XDG

--- pkg/actions/bridge/zsh.go
+++ pkg/actions/bridge/zsh.go
@@ -2,7 +2,6 @@ package bridge
 
 import (
 	_ "embed"
-	"fmt"
 	"strings"
 
 	"github.com/rsteube/carapace"
@@ -19,17 +18,20 @@ func ActionZsh(command ...string) carapace.Action {
 			return carapace.ActionMessage("missing argument [ActionZsh]")
 		}
 
-		configDir, err := xdg.UserConfigDir()
-		if err != nil {
-			return carapace.ActionMessage(err.Error())
-		}
-
-		args := []string{"-c", zsh.Script, "--"}
+		args := []string{"--no-rcs", "-c", zsh.Script, "--"}
 		args = append(args, command...)
 		args = append(args, c.Args...)
 		args = append(args, c.CallbackValue)
 
-		c.Setenv("XDG_CONFIG_HOME", fmt.Sprintf("%v/carapace/bridge", configDir))
+		configDir, err := xdg.UserConfigDir()
+		if err != nil {
+			return carapace.ActionMessage(err.Error())
+		}
+		if err := ensureExists(configDir + "/carapace/bridge/zsh/.zshrc"); err != nil {
+			return carapace.ActionMessage(err.Error())
+		}
+
+		c.Setenv("CARAPACE_BRIDGE_CONFIG_HOME", configDir)
 		return carapace.ActionExecCommand("zsh", args...)(func(output []byte) carapace.Action {
 			lines := strings.Split(string(output), "\r\n")
 			vals := make([]string, 0)
