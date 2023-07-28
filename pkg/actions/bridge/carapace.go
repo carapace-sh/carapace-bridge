6f331f1506ccd20340cca0c3103aa40589273347
rsteube <rsteube@users.noreply.github.com>
2023-07-28 16:51:36 +0200

    ActionCarapaceBin: support full command completion when no argument passed

--- pkg/actions/bridge/carapace.go
+++ pkg/actions/bridge/carapace.go
@@ -2,6 +2,7 @@ package bridge
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
 )
 
 // ActionCarapace bridges https://github.com/rsteube/carapace
@@ -26,20 +27,44 @@ func ActionCarapace(command ...string) carapace.Action {
 
 // ActionCarapaceBin bridges completions registered in carapace-bin
 func ActionCarapaceBin(command ...string) carapace.Action {
-	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
-		if len(command) == 0 {
-			return carapace.ActionMessage("missing argument [ActionCarapaceBin]")
-		}
-
-		args := []string{command[0], "export", ""}
-		args = append(args, command[1:]...)
-		args = append(args, c.Args...)
-		args = append(args, c.Value)
-		return carapace.ActionExecCommand("carapace", args...)(func(output []byte) carapace.Action {
-			if string(output) == "" {
-				return carapace.ActionFiles()
-			}
-			return carapace.ActionImport(output)
+	return actionCommand(command...)(func(command ...string) carapace.Action {
+		return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+			args := []string{command[0], "export", ""}
+			args = append(args, command[1:]...)
+			args = append(args, c.Args...)
+			args = append(args, c.Value)
+			return carapace.ActionExecCommand("carapace", args...)(func(output []byte) carapace.Action {
+				if string(output) == "" {
+					return carapace.ActionFiles()
+				}
+				return carapace.ActionImport(output)
+			})
 		})
 	})
+}
+
+// TODO could use some rework - e.g. name clashing might be a little annoying
+func actionCommand(command ...string) func(f func(command ...string) carapace.Action) carapace.Action {
+	return func(f func(s ...string) carapace.Action) carapace.Action {
+		return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+			if len(command) > 0 {
+				return f(command...)
+			}
+
+			cmd := &cobra.Command{DisableFlagParsing: true}
+			carapace.Gen(cmd).Standalone()
+			carapace.Gen(cmd).PositionalCompletion(
+				carapace.Batch(
+					carapace.ActionExecutables(),
+					carapace.ActionFiles(),
+				).ToA(),
+			)
+			carapace.Gen(cmd).PositionalAnyCompletion(
+				carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+					return f(c.Args[0]).Shift(1)
+				}),
+			)
+			return carapace.ActionExecute(cmd)
+		})
+	}
 }
