118a149f443642ee549b8b4bff92e554331b54f6
rsteube <rsteube@users.noreply.github.com>
2024-10-24 20:39:02 +0200

    include snaphot date in version

--- cmd/carapace-bridge/main.go
+++ cmd/carapace-bridge/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "github.com/carapace-sh/carapace-bridge/cmd/carapace-bridge/cmd"
+import (
+	"fmt"
+	"strings"
 
+	"github.com/carapace-sh/carapace-bridge/cmd/carapace-bridge/cmd"
+)
+
+var date string
 var version = "develop"
 
 func main() {
+	if strings.HasSuffix(version, "-next") {
+		version += fmt.Sprintf(" (%v)", date)
+	}
 	cmd.Execute(version)
 }
